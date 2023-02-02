8fce39d2c0e69e57ea05e645a162fb54b2128bf5
opcc <opcc22059@gmail.com>
2023-02-02 16:35:33 +0800

    update

--- example/swapQuery/preSwapX2YWithOnlineData.ts
+++ example/swapQuery/preSwapX2YWithOnlineData.ts
@@ -6,7 +6,10 @@ import http, { AgentOptions } from 'http'
 import https from 'https'
 import { getLiquidities, getLimitOrders, getPointDelta, getPoolContract, getPoolState } from '../../src/pool/funcs';
 import { getPoolAddress, getLiquidityManagerContract } from '../../src/liquidityManager/view';
-import { amount2Decimal, fetchToken, getErc20TokenContract } from '../../src/base/token/token';
+import { Orders } from '../../src/swapQuery/library/Orders'
+import { LogPowMath } from '../../src/swapQuery/library/LogPowMath'
+import { iZiSwapPool } from '../../src/swapQuery/iZiSwapPool'
+import { amount2Decimal, decimal2Amount, fetchToken, getErc20TokenContract } from '../../src/base/token/token';
 import { pointDeltaRoundingDown, pointDeltaRoundingUp, priceDecimal2Point } from '../../src/base/price';
 import { BigNumber } from 'bignumber.js'
 import { calciZiLiquidityAmountDesired } from '../../src/liquidityManager/calc';
@@ -15,20 +18,17 @@ import { getQuoterContract, quoterSwapChainWithExactInput, quoterSwapChainWithEx
 import { QuoterSwapChainWithExactInputParams, QuoterSwapChainWithExactOutputParams } from '../../src/quoter/types';
 import { getSwapChainWithExactOutputCall, getSwapContract } from '../../src/swap/funcs';
 import { SwapChainWithExactOutputParams } from '../../src/swap/types';
+import { SwapQuery } from '../../src/swapQuery/library/State';
+import { SwapX2YModule } from '../../src/swapQuery/swapX2YModule'
+import JSBI from 'jsbi';
 
 async function main(): Promise<void> {
     const chain:BaseChain = initialChainTable[ChainId.BSCTestnet]
     // test net
     const rpc = 'https://data-seed-prebsc-1-s3.binance.org:8545/'
     console.log('rpc: ', rpc)
-    const options = {
-        agent: {
-            http: new http.Agent({} as AgentOptions),
-            https: new https.Agent({} as AgentOptions),
-            baseUrl: 'http://127.0.0.1:1087'
-        }
-    }
-    const web3 = new Web3(new Web3.providers.HttpProvider(rpc/*, options*/))
+    
+    const web3 = new Web3(new Web3.providers.HttpProvider(rpc))
 
     const liquidityManagerAddress = '0xDE02C26c46AC441951951C97c8462cD85b3A124c'
     const liquidityManagerContract = getLiquidityManagerContract(liquidityManagerAddress, web3)
@@ -72,6 +72,35 @@ async function main(): Promise<void> {
     console.log('limitOrderData.sellingXPoint: ', limitOrderData.sellingXPoint)
     console.log('limitOrderData.sellingY: ', limitOrderData.sellingY)
     console.log('limitOrderData.sellingYPoint: ', limitOrderData.sellingYPoint)
+
+    const orders: Orders.Orders = {
+        liquidity: liquidityData.liquidities,
+        liquidityDeltaPoint: liquidityData.point,
+        sellingX: limitOrderData.sellingX,
+        sellingXPoint: limitOrderData.sellingXPoint,
+        sellingY: limitOrderData.sellingY,
+        sellingYPoint: limitOrderData.sellingYPoint
+    }
+
+    const sqrtRate_96 = LogPowMath.getSqrtPrice(1)
+
+    const swapQueryState: SwapQuery.State = {
+        currentPoint: state.currentPoint,
+        liquidity: JSBI.BigInt(state.liquidity),
+        sqrtPrice_96: LogPowMath.getSqrtPrice(state.currentPoint),
+        liquidityX: JSBI.BigInt(state.liquidityX)
+    }
+
+    const pool = new iZiSwapPool(swapQueryState, orders, sqrtRate_96, pointDelta, fee)
+    
+    const lowPt = state.currentPoint - 1500;
+
+    const inputAmountStr = decimal2Amount(5, iZi).toFixed(0)
+
+    const {amountX, amountY} = SwapX2YModule.swapX2Y(pool, JSBI.BigInt(inputAmountStr), lowPt)
+    
+    console.log('cost: ', amountX.toString())
+    console.log('acquire: ', amountY.toString())
 
 }
 
